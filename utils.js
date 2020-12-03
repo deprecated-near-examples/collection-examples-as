8d2b01400faacf0b1b9191e554f6d7511b7f99a6
Josh Ford <thisisjoshford@gmail.com>
2020-12-03 00:13:45 -0800

    fix key location

--- utils.js
+++ utils.js
@@ -2,18 +2,17 @@ require("dotenv").config({ path: './neardev/dev-account.env' });
 const userHome = require('user-home');
 const nearAPI = require("near-api-js");
 
-const keyStore = new nearAPI.keyStores.UnencryptedFileSystemKeyStore(`${userHome}/.near-credentials`);
-const networkId = 'testnet';
+const keyStore = new nearAPI.keyStores.UnencryptedFileSystemKeyStore(`${userHome}/.near-credentials/`);
 const contractName = process.env.CONTRACT_NAME;
 
 async function contractConnect() {
     const config = {
-        networkId,
         keyStore,
-        nodeUrl: `https://rpc.${networkId}.near.org`,
-        walletUrl: `https://wallet.${networkId}.near.org`,
-        helperUrl: `https://helper.${networkId}.near.org`,
-        explorerUrl: `https://explorer.${networkId}.near.org`
+        networkId: 'default',
+        nodeUrl: 'https://rpc.testnet.near.org',
+        walletUrl: 'https://wallet.testnet.near.org',
+        helperUrl: 'https://helper.testnet.near.org',
+        explorerUrl: 'https://explorer.testnet.near.org'
     }
     
     const connection = await nearAPI.connect(config);
@@ -30,7 +29,8 @@ async function contractConnect() {
 async function setKeyValue(key, value) {
     const contract = await contractConnect();
     try {
-        console.log(`Calling contract [ ${contractName} ] and setting value [ ${value} ]`)
+        console.log(`Calling contract [ ${contractName} ]`)
+        console.log(`Storing { ${key}: ${value} } ...`)
         console.log('---------------------------------------------------------------------------')
         await contract.setValue({ key, value });
         console.log('---------------------------------------------------------------------------')
@@ -41,7 +41,7 @@ async function setKeyValue(key, value) {
 
 async function getValue(key) {
     const contract = await contractConnect();
-    console.log(`Retrieving stored value for [ ${key} ]`);
+    console.log(`Retrieving stored value for "${key}":`);
     result = await contract.getValue({ key });
     console.log('Result:', [ result ])
     console.log('---------------------------------------------------------------------------')
